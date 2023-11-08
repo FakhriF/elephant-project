cf7d4a25b1806a9c87275c755cb3322327b8433a
Zen <109281756+Gibs14@users.noreply.github.com>
2023-11-08 19:32:30 +0700

    Player.gd

--- Class/Player.gd
+++ Class/Player.gd
@@ -1,5 +1,6 @@
 
 class_name Player
+
 var charName
 var description
 var appearance
@@ -7,6 +8,24 @@ var health
 var energy
 var movement
 var player
+
+var character_name = ""
+var character_sprite = null
+var character_attributes = {}
+
+# Initialize the character with the provided attributes
+func _init(name: String, sprite_path: String, attributes: Dictionary):
+	character_name = name
+	character_sprite = load(sprite_path)
+	character_attributes = attributes
+
+func basic_attack():
+	# Add your basic attack logic here
+	var enemy_hp = character_attributes["HP"]
+	enemy_hp -= 10
+	character_attributes["HP"] = enemy_hp
+	print("Using Basic Attack skill! Enemy HP: ", enemy_hp)
+	
 
 func useSkill():
 	pass
